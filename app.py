79339956ad345b334f1f3c2b0a755a9d7e9d714e
guilleeh <guilleeh@uci.edu>
2020-01-27 18:07:17 -0800

    Working

--- app.py
+++ app.py
@@ -1,27 +1,32 @@
-from flask import Flask , render_template
+from flask import Flask, render_template
 import sqlite3
-app = Flask(__name__, static_folder="../static/dist",
-            template_folder="../static")
+app = Flask(__name__)
+
 
 @app.route("/")
 def index():
-    return "hello"
+    return render_template('home.html')
+
 
 @app.route("/create")
 def create():
     return "Create"
 
+
 @app.route("/read")
 def read():
     return "Read"
+
 
 @app.route("/update")
 def update():
     return "Update"
 
+
 @app.route("/delete")
 def delete():
     return "Delete"
+
 
 if __name__ == "__main__":
     app.run()
