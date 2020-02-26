0a7cd1c30fcfa413938071532790e3b9e3b0a617
MantaRey <reymunme@uci.edu>
2020-02-25 19:24:57 -0800

    Rough Draft for Form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 app = Flask(__name__)
 
@@ -26,6 +26,19 @@ def update():
 def success_page():
     return render_template("thank-you.html")
 
+@app.route("/register", methods=('GET','POST'))
+def register_page():
+    print("Can You See This")
+    if request.method == 'POST':
+        firstName = request.form['firstName']
+        lastName = request.form['lastName']
+        email = request.form['email']
+        major = request.form['major']
+        year = request.form['year']
+        username = firstName + lastName
+        print(username)
+        return render_template("thank-you.html")
+    return render_template("register.html")
 
 @app.route("/delete")
 def delete():
