0ca7cb3dd38a8211400fe76f1e7976ff61f0b588
Guvn0 <23511851+Guvn0@users.noreply.github.com>
2020-04-10 19:37:01 -0700

    Added google sheets api and usage

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 from flask import Flask, render_template, request
 import sqlite3
+import sheets_api
 app = Flask(__name__)
 
 
 @app.route("/", methods=('GET', 'POST'))
 def index():    
     if request.method == 'POST':
-        email = request.form["emailInput"]                
+        email = request.form["emailInput"]   
+         
+         
+        sheets_api.sign_in()
+        #Find and AddNew take 2 API calls each
+        #Option 1: Search for email, if exists say already signed in otherwise add the email
+        if sheets_api.find(email) != -1:
+        	print("This email already exists")
+        else:
+        	sheets_api.addNew(email)
+        #Option 2: Add email to sheet, spreadsheet has separate list of unique email
+        #####sheets_api.addNew(email)    
+        #Option 3: Read email list into python and check if unique
+        #####...        	
+        	 
+        	      
         return render_template('thank-you.html')
     return render_template('home.html')
 
