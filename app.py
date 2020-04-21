b9bf1f281156b858573a70b3ca1652b7405d94b9
Guvn0 <23511851+Guvn0@users.noreply.github.com>
2020-04-20 18:46:16 -0700

    Update to google-sheets API on sign-in, includes a buffer of sign-in data that is stored on the server before it is sent to the google sheet. Also uses new credentials file made to keep data secure, supplied in Slack. Include credentials file in sheets folder to use it.

--- app.py
+++ app.py
@@ -1,12 +1,79 @@
 from flask import Flask, render_template, request
+from apscheduler.schedulers.background import BackgroundScheduler
+import atexit
 import sqlite3
+import sheets_api
+
 app = Flask(__name__)
+scheduler = None
+data_buffer = sheets_api.buffer()
+
+#empty a non-empty buffer to sheet
+def flush_buffer():
+    global data_buffer
+    if(len(data_buffer)!=0):
+        print("Flushing buffer")
+        data = data_buffer.process()
+        try:
+            sendData(data)
+        except Exception as e:
+            print("Error occured:\n\t", e)
+            for item in data:
+                data_buffer.add(item)
+            return False
+    return True
+    
+#make sure we have access to sheets and then write it to sheet
+def sendData(data):
+    sheets_api.sign_in()
+    sheets_api.addMultiple(data)
+    
+#make sure that all cancelled, the buffer has been successfully
+def shutdown():
+    print("\nShutting down server...")
+    for job in scheduler.get_jobs():
+        job.remove()
+    print("Flushing buffer")
+    if(not flush_buffer()):
+        print("Flush failed!\n\tcurrent state of data_buffer:", data_buffer)
+    print("Killing scheduler")
+    scheduler.shutdown()
+    print("Shutdown.")
+        
+@app.before_first_request
+def init_scheduler():
+    print("Server starting up...")
+    sheets_api.sign_in()
+
+    global scheduler
+    scheduler = BackgroundScheduler()
+    scheduler.start()
+    
+    #add a job to infrequently flush the buffer if there is data but not
+    #enough to fill the buffer
+    scheduler.add_job(name='Timed flush', id="job_1", func=flush_buffer, trigger='interval', hours=1)
+    
+    # Schedule shutdown to occur when exiting the app
+    atexit.register(lambda: shutdown())
+    print("Server started")
 
 
 @app.route("/", methods=('GET', 'POST'))
-def index():    
+def index():
     if request.method == 'POST':
-        email = request.form["emailInput"]                
+        email = request.form["emailInput"]   
+         
+        global data_buffer
+        data_buffer.add(email)
+        print("data buffer:", data_buffer)
+        
+        
+        #if our buffer has enough data in it, we'll just flush it to sheets
+        if(data_buffer.is_filled()):
+            #we flush it as a job to make sure it doesnt slow anyones responses
+            global scheduler
+            scheduler.add_job(name='Filled flush', id="job_2", func=flush_buffer, trigger='date')        	 
+        	      
         return render_template('thank-you.html')
     return render_template('home.html')
 
@@ -33,6 +100,7 @@ def success_page():
 @app.route("/delete")
 def delete():
     return "Delete"
+
 
 
 if __name__ == "__main__":
---
+++ sheets_api.py
@@ -0,0 +1,135 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+#import datetime
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+# Which Spreadsheet to access
+sheet_name = "test"
+SHEET_ID = '1wGp2kCMkAYo31s4dTlfi8lrMfWsedWT-HV_ZpwsdsCg'
+
+# Runtime API sign-in information
+service = None
+creds = None
+
+'''
+	Reads in data from a spreadsheet at a specific sheet and range
+'''
+def _read(Spreadsheet, Sheet, Range):
+    values = service.spreadsheets().values()
+    result = values.get(spreadsheetId=Spreadsheet,range=Sheet+"!"+Range).execute()
+    return result.get('values', [])
+'''
+	Writes data a spreadsheet at a specific sheet and range
+'''
+def _write(Spreadsheet, Sheet, Range, Data):
+    body = {'values': Data}
+    values = service.spreadsheets().values()
+    result = values.update(spreadsheetId=Spreadsheet, range=Sheet+"!"+Range, valueInputOption="RAW", body=body).execute()
+    
+'''
+	Server-side sign-in, should only force server to sign-in once when token.pickle is invalid or non-existant, so far only my UCI account is allowed to do so. I believe that if you test this with the token.pickle and credentials.json supplied, you won't need to sign-in to my account anyway, link to accessing the spreadsheet is supplied and shared to all UCI members witht the link
+'''
+def sign_in():
+    global service, creds
+    if(creds!=None):
+        print(creds.refresh_token)    
+    if service != None:
+        if(creds.expired):
+            print("Credentials have expired, refreshing credentials and service")
+            creds.refresh(Request())
+            with open('sheets/token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+            service = build('sheets', 'v4', credentials=creds)   
+        return
+    
+    if os.path.exists('sheets/token.pickle'):
+        print("Loading credentials from saved pickle")
+        with open('sheets/token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    
+    if not creds or not creds.valid:
+        print("Credentials don't exist or have expired")
+        #creds.expired updates datetime.timedelta(hours=7, minutes=5) from expiration
+        if creds and creds.expired and creds.refresh_token:
+            print("Using refresh token to gain access token")
+            creds.refresh(Request())
+        else:
+            print("Performing first time token generation")
+            flow = InstalledAppFlow.from_client_secrets_file('sheets/credentials.json', SCOPES)
+            creds = flow.run_local_server(host='localhost', port=8080, prompt='consent')
+            
+        with open('sheets/token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+				
+    service = build('sheets', 'v4', credentials=creds)
+	
+'''
+	Add new data to end of spreadsheet
+	
+	 - only adds 1 item at a time, currently inefficient and not ideal if we send multiple at once, easy to change but unsure of yet
+	 - First reads static location to get where current end of list is, then writes the data at that location
+	 - addmultiple adds multiple pieces of data to the end of the spreadsheet
+'''
+def addSingle(Data):
+    add_pos = _read(SHEET_ID, sheet_name, "B1")[0][0]
+    _write(SHEET_ID, sheet_name, add_pos, [[Data]])
+
+def addMultiple(Data):
+    add_pos = _read(SHEET_ID, sheet_name, "B1")[0][0]
+    _write(SHEET_ID, sheet_name, add_pos, Data)
+'''
+	Find whether data exists and where it exists
+	
+	 - write to static cell location, then read from neighboring cell which gives row number of data or '-1' if the data was not found
+'''
+def find(Data):
+    _write(SHEET_ID, sheet_name, "B2", [[Data]])
+    return int(_read(SHEET_ID, sheet_name, "B3")[0][0])
+	
+'''
+	Special set data structure, to make sure we can add our data and
+	return it in a good format for addMultiple
+'''
+class buffer:
+    def __init__(self):
+        self.data = set()
+    def __str__(self):
+        return str(self.data)
+    def __len__(self):
+        return len(self.data)
+    def add(self, item):
+        if(type(item)==list):
+            item = tuple(item)
+        elif(type(item)!=tuple):
+       	    item = (item,)
+        self.data.add(item)
+    def is_filled(self):
+        #TODO we can change this value to be bigger, smaller values are just faster to test
+        return len(self)>=3
+    def process(self):
+        data = self.data
+        self.__init__()
+
+        data = list(data)
+        for i in range(len(self)):
+            data[i] = list(data[i])
+        return data
+   
+
+if __name__ == '__main__':
+    import sys
+    print(sys.argv)
+	
+	
+	
+	
+	
+	
+	
+	
