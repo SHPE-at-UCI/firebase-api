4237171bc6645d5f325738626db08b726a40c3bc
Guvn0 <23511851+Guvn0@users.noreply.github.com>
2020-04-27 21:12:50 -0700

    Included uci webauth service to make sign in easier for people with uci accounts. In order to be able to use webauth with this site it needs to be hosted on a ".uci.edu" domain, runapp.sh is modified with "export FLASK_RUN_HOST=shpe.uci.edu" in order to account for this, however it seems that to run server locally and work correctly you will need to modify local hosts file: in Linux systems this is found at "/etc/hosts"; in Windows systems it is found at "C:\Windows\System32\Drivers\etc\hosts"; not sure about Mac systems, but it maybe be same as Linux. Inside the hosts file you should see "127.0.0.1	 localhost" if you change this to "127.0.0.1	shpe.uci.edu#localhost" the sign in should work with no issues. Note: shpe in shpe.uci.edu can be replaced with whatever else you choose but I thought it looked more official than anything else I could come up with so I kept it.

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, make_response
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode
 import sqlite3
+import requests
+
 app = Flask(__name__)
 
 
@@ -20,7 +24,6 @@ def create():
 def read():
     return "Read"
 
-
 @app.route("/update")
 def update():
     return "Update"
@@ -33,7 +36,70 @@ def success_page():
 @app.route("/delete")
 def delete():
     return "Delete"
+    
+def refresh_login_status():
+    login_status = {}
+    uci_cookie = "ucinetid_auth"
+    
+    if(uci_cookie in request.cookies):
+        param = urlencode({uci_cookie: request.cookies.get(uci_cookie)})
+        webauth_check = 'http://login.uci.edu/ucinetid/webauth_check'
+        resp = requests.get(webauth_check+"?"+param).content.decode("utf-8")
+        
+        for line in resp.split("\n"):
+            key_val = line.split("=")
+            if(len(key_val)!=2 or key_val[1]==''):
+                continue
+            elif(len(key_val)==2 and key_val[0]=="uci_affiliations"):
+                login_status[key_val[0]] = set(key_val[1].split(","))
+            else:
+                login_status[key_val[0]] = key_val[1]
+                
+        login_status['valid'] = False
+        if('error_code' in login_status or 'auth_fail' in login_status):
+            print("Error Code:",login_status.get('error_codes'))
+            print("Authentication Failure:",login_status.get('auth_fail'))
+        elif('ucinetid' in login_status and 'time_created' in login_status and login_status['auth_host'] == login_status['x_forwarded_for']):
+            login_status['valid'] = True
+        
+    return login_status    
+             
 
+@app.route("/login")
+def login():
+    resp = None
+    param = urlencode({"return_url": "http://shpe.uci.edu:5000"})
+    webauth = 'http://login.uci.edu/ucinetid/webauth?' + param
+    
+    login_status = refresh_login_status()
+    if(login_status['valid']):
+        return "Already logged in as "+login_status['ucinetid']#redirect("/")
+        #resp = make_response(redirect("/"))
+        #resp.set_cookie('SHPE', 'logged in')
+    else:
+        return redirect(webauth)q
+        #resp = make_response(redirect(webauth))
+        #resp.set_cookie('SHPE', 'login')
+    #return resp
+    
+@app.route("/logout")
+def logout():
+    resp = None
+    param = urlencode({"return_url": "http://shpe.uci.edu:5000"})
+    webauth = 'http://login.uci.edu/ucinetid/webauth_logout?' + param
+    
+    login_status = refresh_login_status()
+    if(login_status['valid']):
+        return redirect(webauth)
+        #resp = make_response(redirect(webauth))
+        #resp.set_cookie('SHPE', 'logout')
+    else:
+        return "Not logged in"#redirect("/")
+        #resp = make_response(redirect("/"))
+        #resp.set_cookie('SHPE', 'logged out')
+    #return resp
+    
+    
 
 if __name__ == "__main__":
     app.run()
--- runapp.sh
+++ runapp.sh
@@ -2,5 +2,6 @@
 
 export FLASK_APP=app.py
 export FLASK_ENV=development
+export FLASK_RUN_HOST=shpe.uci.edu
 
 flask run
