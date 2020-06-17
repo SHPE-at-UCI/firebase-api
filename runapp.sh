09183cebac2a52a9982781c5036aa65f611ba63f
Guvn0 <23511851+Guvn0@users.noreply.github.com>
2020-06-17 00:13:55 -0700

    Commented out all options for modifying the host-name and the flask host variable.

--- runapp.sh
+++ runapp.sh
@@ -19,12 +19,12 @@ export FLUSH_MINUTES=1
 #copy of parameters
 APP=$FLASK_APP
 ENV=$FLASK_ENV
-HOST=$FLASK_RUN_HOST
+#HOST=$FLASK_RUN_HOST
 LIMIT=$FLUSH_LIMIT
 HOURS=$FLUSH_HOURS
 MINUTES=$FLUSH_MINUTES
 
-while getopts '?pa:e:r:l:h:m:' OPTION; do
+while getopts '?pa:e:l:h:m:' OPTION; do #'?pa:e:r:l:h:m:' OPTION; do
   var=$(printf "%d\n" $OPTARG 2>/dev/null)
   case "$OPTION" in
     a)
@@ -35,10 +35,10 @@ while getopts '?pa:e:r:l:h:m:' OPTION; do
         if [ "$OPTARG" = "development" ]||[ "$OPTARG" = "production" ]; then
             ENV=$OPTARG
         fi;;
-    r)
-        if [ -n "$OPTARG" ]; then
-            HOST=$OPTARG
-        fi;;
+    # r)
+    #     if [ -n "$OPTARG" ]; then
+    #         HOST=$OPTARG
+    #     fi;;
     l)
         if [ $var -gt 0 ]; then
             LIMIT=$OPTARG
@@ -54,7 +54,8 @@ while getopts '?pa:e:r:l:h:m:' OPTION; do
     p)
         print=true;;
     ?)
-        echo "Usage: $0 [-?] [-a app-name] [-e environ] [-r host-name] [-p] [-l size] [-h hours] [-m minutes]"
+        echo "Usage: $0 [-?] [-a app-name] [-e environ] [-p] [-l size] [-h hours] [-m minutes]"
+        #echo "Usage: $0 [-?] [-a app-name] [-e environ] [-r host-name] [-p] [-l size] [-h hours] [-m minutes]"
         echo "Run flask app '$FLASK_APP' in '$FLASK_ENV' environment with hostname '$FLASK_RUN_HOST' (by default)."
         echo ""
         echo "Current defaults are:"
@@ -67,7 +68,7 @@ while getopts '?pa:e:r:l:h:m:' OPTION; do
         echo "  -p \t\t print out values of parameters before running"
         echo "  -a app-name \t run using a different flask app"
         echo "  -e environ \t run using different flask environment (development or production)"
-        echo "  -r host-name \t run using different flask hostname"
+        #echo "  -r host-name \t run using different flask hostname"
         echo "  -l size \t change how much data is needed before buffer flushing (>=1)"
         echo "  -h hours \t flush the buffer every so hours (>=0)"
         echo "  -m minutes \t flush the buffer every so minutes (>=0)"
@@ -78,7 +79,7 @@ done
 #write back out parameters
 FLASK_APP=$APP
 FLASK_ENV=$ENV
-FLASK_RUN_HOST=$HOST
+#FLASK_RUN_HOST=$HOST
 FLUSH_LIMIT=$LIMIT
 FLUSH_HOURS=$HOURS
 FLUSH_MINUTES=$MINUTES
