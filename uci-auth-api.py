66516b395b4bfdd5c0e9264ed272a966198c28aa
Josue Lopez <josueal1@uci.edu>
2021-05-16 04:05:22 +0000

    fix(uci-auth-api): refactor and format

--- uci-auth-api.py
+++ uci-auth-api.py
@@ -5,25 +5,23 @@ import requests
 
 
 class UciAuthApi:
-    def __init__(self):
-        sheetsApi = SheetsApiClient()
+    def __init(self):
+        self.sheetsApi = SheetsApiClient()
 
     def uci_signin(self):
-        resp = None
         param = urlencode({"return_url": "http://shpe.uci.edu:5000/login"})
         webauth = "http://login.uci.edu/ucinetid/webauth?" + param
 
-        login_status = refresh_login_status()
+        login_status = self.refresh_login_status()
         if not login_status:
             return "UCI login failed"
         elif login_status["valid"]:
             print(login_status)
-            return sheetsApi.signin_user(login_status["ucinetid"])
+            return self.sheetsApi.signin_user(login_status["ucinetid"])
         else:
             return webauth
 
     def logout(self):
-        resp = None
         param = urlencode({"return_url": "http://shpe.uci.edu:5000/logout"})
         webauth = "http://login.uci.edu/ucinetid/webauth_logout?" + param
 
@@ -42,11 +40,12 @@ class UciAuthApi:
         login_status = None
         uci_cookie = "ucinetid_auth"
 
-        if uci_cookie in request.cookies:
+        if uci_cookie in requests.cookies:
             login_status = {}
-            param = urlencode({uci_cookie: request.cookies.get(uci_cookie)})
+            param = urlencode({uci_cookie: requests.cookies.get(uci_cookie)})
             webauth_check = "http://login.uci.edu/ucinetid/webauth_check"
-            resp = requests.get(webauth_check + "?" + param).content.decode("utf-8")
+            resp = requests.get(webauth_check + "?" + param)
+            resp = resp.content.decode("utf-8")
 
             for line in resp.split("\n"):
                 key_val = line.split("=")
@@ -58,14 +57,20 @@ class UciAuthApi:
                     login_status[key_val[0]] = key_val[1]
 
             login_status["valid"] = False
-            if "error_code" in login_status or "auth_fail" in login_status:
+
+            theres_err_code = "error_code" in login_status
+            theres_auth_fail = "auth_fail" in login_status
+            theres_ucinetid = "ucinetid" in login_status
+            theres_timestamp = "time_created" in login_status
+            valid_auth_host = (
+                login_status["auth_host"] == login_status["x_forwarded_for"]
+            )
+
+            if theres_err_code or theres_auth_fail:
                 print("Error Code:", login_status.get("error_codes"))
                 print("Authentication Failure:", login_status.get("auth_fail"))
-            elif (
-                "ucinetid" in login_status
-                and "time_created" in login_status
-                and login_status["auth_host"] == login_status["x_forwarded_for"]
-            ):
+
+            elif theres_ucinetid and theres_timestamp and valid_auth_host:
                 login_status["valid"] = True
 
         return login_status
