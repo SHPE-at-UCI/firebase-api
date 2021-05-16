65fa91afab5ff2ace429ab85c0bf3f8ac470c0ea
Josue Lopez <josueal1@uci.edu>
2021-05-15 20:29:31 -0700

    fix(uci-auth-api.py): inconsistent use of tabs

--- uci-auth-api.py
+++ uci-auth-api.py
@@ -31,33 +31,30 @@ def logout():
         return redirect("/")
 
 
+def refresh_login_status():
+	login_status = None
+	uci_cookie = "ucinetid_auth"
 
-	def refresh_login_status():
-	    login_status = None
-	    uci_cookie = "ucinetid_auth"
+	if uci_cookie in request.cookies:
+		login_status = {}
+		param = urlencode({uci_cookie: request.cookies.get(uci_cookie)})
+		webauth_check = "http://login.uci.edu/ucinetid/webauth_check"
+		resp = requests.get(webauth_check + "?" + param).content.decode("utf-8")
 
-	    if uci_cookie in request.cookies:
-	        login_status = {}
-	        param = urlencode({uci_cookie: request.cookies.get(uci_cookie)})
-	        webauth_check = "http://login.uci.edu/ucinetid/webauth_check"
-	        resp = requests.get(webauth_check + "?" +
-	                            param).content.decode("utf-8")
+		for line in resp.split("\n"):
+			key_val = line.split("=")
+			if len(key_val) != 2 or key_val[1] == "":
+				continue
+			elif len(key_val) == 2 and key_val[0] == "uci_affiliations":
+				login_status[key_val[0]] = set(key_val[1].split(","))
+			else:
+				login_status[key_val[0]] = key_val[1]
 
-	        for line in resp.split("\n"):
-	            key_val = line.split("=")
-	            if len(key_val) != 2 or key_val[1] == "":
-	                continue
-	            elif len(key_val) == 2 and key_val[0] == "uci_affiliations":
-	                login_status[key_val[0]] = set(key_val[1].split(","))
-	            else:
-	                login_status[key_val[0]] = key_val[1]
+		login_status["valid"] = False
+		if "error_code" in login_status or "auth_fail" in login_status:
+			print("Error Code:", login_status.get("error_codes"))
+			print("Authentication Failure:", login_status.get("auth_fail"))
+		elif ("ucinetid" in login_status and "time_created" in login_status and login_status["auth_host"] == login_status["x_forwarded_for"]):
+			login_status["valid"] = True
 
-	        login_status["valid"] = False
-	        if "error_code" in login_status or "auth_fail" in login_status:
-	            print("Error Code:", login_status.get("error_codes"))
-	            print("Authentication Failure:", login_status.get("auth_fail"))
-	        elif ("ucinetid" in login_status and "time_created" in login_status and
-	              login_status["auth_host"] == login_status["x_forwarded_for"]):
-	            login_status["valid"] = True
-
-	    return login_status
+	return login_status
