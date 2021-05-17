26b13e39f6291a28339ac3fd6ac76a3b9d4791b0
Josue Lopez <josueal1@uci.edu>
2021-05-16 17:21:42 -0700

    (MOCK_DATA.py): add so entry devs can test locally

---
+++ MOCK_DATA.py
@@ -0,0 +1,497 @@
+# MOCK_DATA.py
+#
+# SHPE UCI Tech Committee
+# Josue Lopez
+#
+# This python file contains a mock data in the same manner that it appears
+# on our Google Firebase Realtime Database
+
+users = {
+    "dgZmjXGJmfbA-hU5DNEJkg": {
+        "email": "irentoll0@uiuc.edu",
+        "first_name": "Imogen",
+        "last_name": "Rentoll",
+        "resume_id": "https://a8.net/rutrum/neque/aenean.xml",
+        "is_admin": False,
+        "major": "Recruiting Manager",
+        "year": "Senior",
+        "paid_status": "general",
+    },
+    "q5aDHPN2xoxUZwGZNt4rcw": {
+        "email": "krubury1@washington.edu",
+        "first_name": "Kelsy",
+        "last_name": "Rubury",
+        "resume_id": "https://bbb.org/sapien/non/mi/integer/ac.xml",
+        "is_admin": False,
+        "major": "Automation Specialist I",
+        "year": 3,
+        "paid_status": "paid",
+    },
+    "ifnW5O6x3mXJZghgISZAyA": {
+        "email": "jglason2@noaa.gov",
+        "first_name": "Joshua",
+        "last_name": "Glason",
+        "resume_id": "https://msu.edu/cubilia/curae/donec.json",
+        "is_admin": True,
+        "major": "General Manager",
+        "year": "Senior",
+        "paid_status": "paid",
+    },
+    "Ryd01rS5OT6ao3W56Hfwcg": {
+        "email": "plaidel3@zimbio.com",
+        "first_name": "Petr",
+        "last_name": "Laidel",
+        "resume_id": "https://mlb.com/auctor.png",
+        "is_admin": True,
+        "major": "Payment Adjustment Coordinator",
+        "year": 2,
+        "paid_status": "general",
+    },
+    "L9mdxX7zfL7qTDfP1clQ6Q": {
+        "email": "ckeneleyside4@hp.com",
+        "first_name": "Chandler",
+        "last_name": "Keneleyside",
+        "resume_id": "http://github.io/sapien/quis/libero/nullam/sit/amet.html",
+        "is_admin": True,
+        "major": "Business Systems Development Analyst",
+        "year": 1,
+        "paid_status": "paid",
+    },
+    "Mm46zESsLh3TzoVsRgWOnQ": {
+        "email": "hmoorey5@1688.com",
+        "first_name": "Huberto",
+        "last_name": "Moorey",
+        "resume_id": "http://prweb.com/iaculis/congue/vivamus/metus/arcu/adipiscing/molestie.jpg",
+        "is_admin": False,
+        "major": "Director of Sales",
+        "year": 2,
+        "paid_status": "general",
+    },
+    "khQldwE-jC5jp8skUjHu3Q": {
+        "email": "lgiddins6@sciencedirect.com",
+        "first_name": "Lorne",
+        "last_name": "Giddins",
+        "resume_id": "http://sphinn.com/ut.js",
+        "is_admin": False,
+        "major": "Information Systems Manager",
+        "year": 3,
+        "paid_status": "general",
+    },
+    "7TBnUpBPi62Lo2XIlf-_lA": {
+        "email": "filyuchyov7@desdev.cn",
+        "first_name": "Farlee",
+        "last_name": "Ilyuchyov",
+        "resume_id": "http://devhub.com/at/velit.aspx",
+        "is_admin": True,
+        "major": "Occupational Therapist",
+        "year": 3,
+        "paid_status": "paid",
+    },
+    "i12Dh_pQx5ClQGAPKCv0Kg": {
+        "email": "wianson8@ehow.com",
+        "first_name": "Wilek",
+        "last_name": "Ianson",
+        "resume_id": "https://sfgate.com/non/mauris.aspx",
+        "is_admin": False,
+        "major": "Environmental Tech",
+        "year": 3,
+        "paid_status": "paid",
+    },
+    "lzilSrKZv68qVfRdbcJIFQ": {
+        "email": "tpadefield9@joomla.org",
+        "first_name": "Trista",
+        "last_name": "Padefield",
+        "resume_id": "http://yale.edu/id/nisl.js",
+        "is_admin": False,
+        "major": "Community Outreach Specialist",
+        "year": 2,
+        "paid_status": "general",
+    },
+    "VZGxsTLx00VtBbwkxSgsbA": {
+        "email": "hfrudea@alibaba.com",
+        "first_name": "Horatia",
+        "last_name": "Frude",
+        "resume_id": "http://berkeley.edu/tellus/nulla/ut/erat.aspx",
+        "is_admin": True,
+        "major": "Analyst Programmer",
+        "year": 1,
+        "paid_status": "paid",
+    },
+    "BsqQvqAhP0q8_ZFd6dEqAw": {
+        "email": "rbesseb@digg.com",
+        "first_name": "Ramona",
+        "last_name": "Besse",
+        "resume_id": "http://cyberchimps.com/placerat/ante/nulla/justo/aliquam/quis/turpis.jsp",
+        "is_admin": False,
+        "major": "Statistician IV",
+        "year": "Senior",
+        "paid_status": "paid",
+    },
+    "vLcnf4lQ1lHsqoV8qUIaAA": {
+        "email": "cfairbanksc@go.com",
+        "first_name": "Cicely",
+        "last_name": "Fairbanks",
+        "resume_id": "http://pen.io/faucibus/accumsan/odio/curabitur.json",
+        "is_admin": True,
+        "major": "Business Systems Development Analyst",
+        "year": 2,
+        "paid_status": "paid",
+    },
+    "5qqJVt4slmI4uAd8iqrU8g": {
+        "email": "gfreckeltond@redcross.org",
+        "first_name": "Giorgio",
+        "last_name": "Freckelton",
+        "resume_id": "https://sogou.com/sed/sagittis/nam.jpg",
+        "is_admin": True,
+        "major": "Mechanical Systems Engineer",
+        "year": "Senior",
+        "paid_status": "general",
+    },
+    "SHi7vXfLYPZDu9HWeuQOJQ": {
+        "email": "nangelinie@google.co.jp",
+        "first_name": "Nadya",
+        "last_name": "Angelini",
+        "resume_id": "http://npr.org/morbi.js",
+        "is_admin": False,
+        "major": "Mechanical Systems Engineer",
+        "year": 1,
+        "paid_status": "paid",
+    },
+    "YtgKD7MPbne_cM97-tHyhA": {
+        "email": "cantonovf@wikimedia.org",
+        "first_name": "Craggy",
+        "last_name": "Antonov",
+        "resume_id": "http://goodreads.com/lacinia/sapien/quis/libero/nullam/sit/amet.png",
+        "is_admin": False,
+        "major": "Engineer III",
+        "year": 2,
+        "paid_status": "paid",
+    },
+    "H42g_rFEOthM5HEo80bQng": {
+        "email": "bmacgoverng@comsenz.com",
+        "first_name": "Boyd",
+        "last_name": "MacGovern",
+        "resume_id": "http://pagesperso-orange.fr/praesent/blandit/nam.js",
+        "is_admin": True,
+        "major": "Account Coordinator",
+        "year": 3,
+        "paid_status": "general",
+    },
+    "XbcQKm000v1e8GO5yoYGPA": {
+        "email": "aattach@hibu.com",
+        "first_name": "Aluin",
+        "last_name": "Attac",
+        "resume_id": "https://go.com/est/lacinia/nisi/venenatis/tristique/fusce/congue.jsp",
+        "is_admin": True,
+        "major": "Programmer Analyst II",
+        "year": "Senior",
+        "paid_status": "paid",
+    },
+    "OahRZyoqKOJ5a11hdAzeLg": {
+        "email": "ldanaheri@symantec.com",
+        "first_name": "Luigi",
+        "last_name": "Danaher",
+        "resume_id": "https://godaddy.com/arcu/adipiscing/molestie.png",
+        "is_admin": False,
+        "major": "VP Accounting",
+        "year": 1,
+        "paid_status": "general",
+    },
+    "HxyfPYRofym2aH6LW3CgUA": {
+        "email": "tdeernessj@scribd.com",
+        "first_name": "Trenton",
+        "last_name": "Deerness",
+        "resume_id": "https://google.ca/amet.png",
+        "is_admin": True,
+        "major": "Nuclear Power Engineer",
+        "year": 2,
+        "paid_status": "general",
+    },
+    "r9bm9j1eWKXa92herp8xVg": {
+        "email": "ajexk@aol.com",
+        "first_name": "Antoine",
+        "last_name": "Jex",
+        "resume_id": "https://weibo.com/a/odio.aspx",
+        "is_admin": False,
+        "major": "Research Nurse",
+        "year": 3,
+        "paid_status": "paid",
+    },
+    "JtUvB8HvSLay4OfRq_D1Qw": {
+        "email": "lhungerfordl@amazon.com",
+        "first_name": "Lidia",
+        "last_name": "Hungerford",
+        "resume_id": "https://moonfruit.com/velit/donec/diam/neque.js",
+        "is_admin": True,
+        "major": "Data Coordiator",
+        "year": 3,
+        "paid_status": "general",
+    },
+    "teDT4b_W0FnOChLOupBFbw": {
+        "email": "ekovelmannm@sogou.com",
+        "first_name": "Emelina",
+        "last_name": "Kovelmann",
+        "resume_id": "http://auda.org.au/justo/sollicitudin.json",
+        "is_admin": True,
+        "major": "Research Nurse",
+        "year": 2,
+        "paid_status": "paid",
+    },
+    "428MPsWKWVq5fHS5C2-Ubw": {
+        "email": "wwinckworthn@ft.com",
+        "first_name": "Weidar",
+        "last_name": "Winckworth",
+        "resume_id": "https://icio.us/dis.js",
+        "is_admin": True,
+        "major": "Sales Associate",
+        "year": "Senior",
+        "paid_status": "general",
+    },
+    "Lljze-4UgFPYGBQ8ZpCbiw": {
+        "email": "laddicoteo@infoseek.co.jp",
+        "first_name": "Launce",
+        "last_name": "Addicote",
+        "resume_id": "https://msn.com/elementum.aspx",
+        "is_admin": True,
+        "major": "Financial Analyst",
+        "year": 1,
+        "paid_status": "paid",
+    },
+    "qCSGcOxMGm66m9TEQUnabg": {
+        "email": "mbignoldp@bizjournals.com",
+        "first_name": "Marla",
+        "last_name": "Bignold",
+        "resume_id": "https://amazon.co.jp/odio/curabitur/convallis.png",
+        "is_admin": False,
+        "major": "Pharmacist",
+        "year": 3,
+        "paid_status": "general",
+    },
+    "rcOmPxw7AHB28O0Rhn4y-w": {
+        "email": "mmcquakerq@jigsy.com",
+        "first_name": "Matty",
+        "last_name": "McQuaker",
+        "resume_id": "https://forbes.com/proin/at/turpis/a/pede/posuere.js",
+        "is_admin": True,
+        "major": "Structural Engineer",
+        "year": "Senior",
+        "paid_status": "general",
+    },
+    "nKi3EthmOIcdaYhcikK9eQ": {
+        "email": "tdrohunr@surveymonkey.com",
+        "first_name": "Tamera",
+        "last_name": "Drohun",
+        "resume_id": "http://github.io/viverra/diam/vitae/quam/suspendisse.png",
+        "is_admin": True,
+        "major": "Food Chemist",
+        "year": 1,
+        "paid_status": "paid",
+    },
+    "6tW2INqaUHNaTaUIR-SCyA": {
+        "email": "jbaruchs@desdev.cn",
+        "first_name": "Jerrie",
+        "last_name": "Baruch",
+        "resume_id": "https://yellowpages.com/sagittis.js",
+        "is_admin": True,
+        "major": "Health Coach I",
+        "year": 1,
+        "paid_status": "paid",
+    },
+    "4lbvfNd4oHGRRY8F4vwgEw": {
+        "email": "hossipenkot@mit.edu",
+        "first_name": "Hazel",
+        "last_name": "Ossipenko",
+        "resume_id": "https://jigsy.com/tincidunt/ante/vel/ipsum.jsp",
+        "is_admin": False,
+        "major": "Accountant III",
+        "year": 2,
+        "paid_status": "general",
+    },
+    "Rmq-_gEtF1_9OszF5HRSJA": {
+        "email": "mmelonbyu@sciencedirect.com",
+        "first_name": "Melania",
+        "last_name": "Melonby",
+        "resume_id": "https://google.de/duis/consequat/dui/nec/nisi/volutpat.json",
+        "is_admin": False,
+        "major": "Graphic Designer",
+        "year": 2,
+        "paid_status": "paid",
+    },
+    "PCi7D9uG6IeXUP5PqK__Nw": {
+        "email": "emcnayv@paginegialle.it",
+        "first_name": "Ekaterina",
+        "last_name": "McNay",
+        "resume_id": "http://t.co/augue/luctus.jpg",
+        "is_admin": True,
+        "major": "Technical Writer",
+        "year": 1,
+        "paid_status": "paid",
+    },
+}
+
+points = {
+   "dgZmjXGJmfbA-hU5DNEJkg":{
+      "fall":0,
+      "winter":3,
+      "spring":4
+   },
+   "q5aDHPN2xoxUZwGZNt4rcw":{
+      "fall":8,
+      "winter":10,
+      "spring":2
+   },
+   "ifnW5O6x3mXJZghgISZAyA":{
+      "fall":5,
+      "winter":10,
+      "spring":3
+   },
+   "Ryd01rS5OT6ao3W56Hfwcg":{
+      "fall":3,
+      "winter":9,
+      "spring":9
+   },
+   "L9mdxX7zfL7qTDfP1clQ6Q":{
+      "fall":6,
+      "winter":3,
+      "spring":8
+   },
+   "Mm46zESsLh3TzoVsRgWOnQ":{
+      "fall":7,
+      "winter":4,
+      "spring":3
+   },
+   "khQldwE-jC5jp8skUjHu3Q":{
+      "fall":3,
+      "winter":8,
+      "spring":2
+   },
+   "7TBnUpBPi62Lo2XIlf-_lA":{
+      "fall":0,
+      "winter":7,
+      "spring":0
+   },
+   "i12Dh_pQx5ClQGAPKCv0Kg":{
+      "fall":4,
+      "winter":5,
+      "spring":7
+   },
+   "lzilSrKZv68qVfRdbcJIFQ":{
+      "fall":3,
+      "winter":4,
+      "spring":0
+   },
+   "VZGxsTLx00VtBbwkxSgsbA":{
+      "fall":7,
+      "winter":8,
+      "spring":0
+   },
+   "BsqQvqAhP0q8_ZFd6dEqAw":{
+      "fall":10,
+      "winter":6,
+      "spring":0
+   },
+   "vLcnf4lQ1lHsqoV8qUIaAA":{
+      "fall":6,
+      "winter":2,
+      "spring":9
+   },
+   "5qqJVt4slmI4uAd8iqrU8g":{
+      "fall":5,
+      "winter":10,
+      "spring":1
+   },
+   "SHi7vXfLYPZDu9HWeuQOJQ":{
+      "fall":2,
+      "winter":3,
+      "spring":10
+   },
+   "YtgKD7MPbne_cM97-tHyhA":{
+      "fall":3,
+      "winter":8,
+      "spring":8
+   },
+   "H42g_rFEOthM5HEo80bQng":{
+      "fall":4,
+      "winter":9,
+      "spring":7
+   },
+   "XbcQKm000v1e8GO5yoYGPA":{
+      "fall":2,
+      "winter":8,
+      "spring":2
+   },
+   "OahRZyoqKOJ5a11hdAzeLg":{
+      "fall":3,
+      "winter":4,
+      "spring":1
+   },
+   "HxyfPYRofym2aH6LW3CgUA":{
+      "fall":4,
+      "winter":5,
+      "spring":5
+   },
+   "r9bm9j1eWKXa92herp8xVg":{
+      "fall":2,
+      "winter":4,
+      "spring":10
+   },
+   "JtUvB8HvSLay4OfRq_D1Qw":{
+      "fall":8,
+      "winter":10,
+      "spring":4
+   },
+   "teDT4b_W0FnOChLOupBFbw":{
+      "fall":1,
+      "winter":10,
+      "spring":0
+   },
+   "428MPsWKWVq5fHS5C2-Ubw":{
+      "fall":10,
+      "winter":4,
+      "spring":3
+   },
+   "Lljze-4UgFPYGBQ8ZpCbiw":{
+      "fall":4,
+      "winter":9,
+      "spring":6
+   },
+   "qCSGcOxMGm66m9TEQUnabg":{
+      "fall":9,
+      "winter":10,
+      "spring":9
+   },
+   "rcOmPxw7AHB28O0Rhn4y-w":{
+      "fall":10,
+      "winter":1,
+      "spring":9
+   },
+   "nKi3EthmOIcdaYhcikK9eQ":{
+      "fall":3,
+      "winter":6,
+      "spring":7
+   },
+   "6tW2INqaUHNaTaUIR-SCyA":{
+      "fall":6,
+      "winter":4,
+      "spring":4
+   },
+   "4lbvfNd4oHGRRY8F4vwgEw":{
+      "fall":4,
+      "winter":3,
+      "spring":8
+   },
+   "Rmq-_gEtF1_9OszF5HRSJA":{
+      "fall":8,
+      "winter":2,
+      "spring":10
+   },
+   "PCi7D9uG6IeXUP5PqK__Nw":{
+      "fall":6,
+      "winter":9,
+      "spring":9
+   }
+}
+
+if __name__ == "__main__":
+    print(users)
+    print(points)
